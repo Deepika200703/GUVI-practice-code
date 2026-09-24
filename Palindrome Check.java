7dfb99ede34fea25a88335258ec0a4993ccdacaa
Deepika200703 <deepikarajas07@gmail.com>
2026-09-24 17:47:34 +0530

    Palindrome Check.java

---
+++ Palindrome Check.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+        String reverse = "";
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            reverse += s.charAt(i);
+        }
+
+        if (s.equals(reverse)) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+    }
+}
